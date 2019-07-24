619fb6b9c6b4d2506c889ae85b0c49de7a1a0601
Paula Alvares <paula.p.alvares@gmail.com>
2019-07-24 10:36:12 -0300

    Implementando os descontos

--- src/test/java/br/com/caelum/ingresso/validacao/GerenciadorDeSessaoTest.java
+++ src/test/java/br/com/caelum/ingresso/validacao/GerenciadorDeSessaoTest.java
@@ -61,7 +61,7 @@ public class GerenciadorDeSessaoTest {
 		
 		List<Sessao> sessoes = Arrays.asList(sessaoDasDez, sessaoDasDezoito);
 		GerenciadorDeSessao gerenciador = new GerenciadorDeSessao(sessoes);
-		Assert.assertFalse(gerenciador.cabe(sessaoDasTreze));
+		Assert.assertTrue(gerenciador.cabe(sessaoDasTreze));
 	}
 	
 	@Test
